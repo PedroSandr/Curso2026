f1ed68765e8780606eae3b7e5c9a96f4e2652704
Pedro <smpedro1988@gmail.com>
2025-12-28 21:34:02 +0100

    Prueba1

---
+++ PrimerPrograma/ejercicio 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Hola Mundo" << endl ;  //Incluye mensaje en la consola
+
+    return 0;
+
+}
---
+++ PrimerPrograma/ejercicio 3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#define salario 15
+
+int main (){
+
+    int arra_matriz [5];
+
+    int arra2_matriz [salario];
+
+
+}
